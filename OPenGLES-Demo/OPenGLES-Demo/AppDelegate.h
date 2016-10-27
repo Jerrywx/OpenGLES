19ca95d6e774a3c60daa7b12baf01a083eff3cee
wxiao <wangxiao117210@zongheng.com>
2016-10-27 13:34:18 +0800

    创建测试工程

---
+++ OPenGLES-Demo/OPenGLES-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OPenGLES-Demo
+//
+//  Created by 王潇 on 16/10/27.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OPenGLES-Demo/OPenGLES-Demo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  OPenGLES-Demo
+//
+//  Created by 王潇 on 16/10/27.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+
+	self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
